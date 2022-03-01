5920a29e6a6a303ec8fe34482ebb6a4bc5698f20
Akshat Shrinate <97226829+Shrinate214@users.noreply.github.com>
2022-03-01 21:17:31 +0530

    Add files via upload

---
+++ Assignment 4/ASSIGN4_output.py
@@ -0,0 +1,66 @@
+Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+========== RESTART: C:\Users\HP\Desktop\Python_assign4_source_code.py ==========
+Let A be the initial rod where the discs are currently present
+Let B is the extra or mid rod
+And C be the final rod where the discs are supposed to reach
+Steps Required to solve the Tower of Hanoi problem for 3 discs :
+Move disk 1 from rod A to rod C
+Move disk 2 from rod A to rod B
+Move disk 1 from rod C to rod B
+Move disk 3 from rod A to rod C
+Move disk 1 from rod B to rod A
+Move disk 2 from rod B to rod C
+Move disk 1 from rod A to rod C
+Number of steps will be: 7
+Enter number of rows to be printed in the pascal triangle problem:5
+The Pascal's Triangle using iteration is:
+    1 
+   1 1 
+  1 2 1 
+ 1 3 3 1 
+1 4 6 4 1 
+The Pascal's Triangle using recusion is:
+    1 
+   1 1 
+  1 2 1 
+ 1 3 3 1 
+1 4 6 4 1 
+Enter the first integer:34
+Enter the second integer:8
+Remainder is: 2
+Quotient is: 4
+Checking whether the function (divmod()) is callable or not:
+It returns true which means it is callable
+Checking whether all the values are zero or not:
+All values in result(quotient and remainder) are non-zero
+On Adding (4,5,6) to the result of division and filtering out values greater than 4:
+The new list formed is: (2, 4, 4, 5, 6)
+Values greater than 4 are: [5, 6]
+Converting the output from list 1into a set datatype:
+The output of previous part in set datatype would be: {5, 6}
+Making the output set of previous part immutable:
+The immutable set would be: frozenset({5, 6})
+Evaluating the maximum value from the set and finding out its hash value:
+The maximum value from the set is: 6
+The hash value of set on considering it to be an integer is: 6
+The hash value of set on considering it as a string is: -4087217822581035354
+Enter name of student:akshat shrinate
+Enter roll number/SID of student:21105010
+Object has been Created
+The name of the student is akshat shrinate and his/her/their roll number is 21105010
+Object has been destroyed
+The current database is as follows:
+Mehak has a salary of 40000 rupees
+Ashok has a salary of 50000 rupees
+Viren has a salary of 60000 rupees
+(i) Salary of Mehak has been updated from 40000 to 70000
+(ii) The record of Viren has been deleted
+The final database is:
+Mehak has a salary of 70000 rupees
+Ashok has a salary of 50000 rupees
+Enter George's word:Akshay
+Enter Barbie's guessed word:kshaay
+Is the word meaningful?( Enter YES or NO):YES
+Congratulations!, you passed the test!!
